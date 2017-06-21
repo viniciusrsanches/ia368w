ca53432045d1c47ed855dc78dacafe2a3292ad16
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-06-20 21:25:09 -0300

    testing new possibilities

--- Atividade4/ekf_slam.m
+++ Atividade4/ekf_slam.m
@@ -2,7 +2,7 @@ close all; clear;
 
 
 addpath('~/Desktop/APIs/Matlab');
-host = 'http://192.168.0.105:4950';
+host = 'http://192.168.0.102:4950';
 laser = '/perception/laser/1/distances?range=-90:90:1';
 
 gr1.name = 'group1';
@@ -19,7 +19,7 @@ b = 165;
 R_t = [25 0 0 ; 0 25 0 ; 0 0 (0.5*pi/180)^2];
 Q_t = [1 0; 0 (0.5*pi/180)^2];
 Sigma = [ 0 0 0; 0 0 0; 0 0 0];
-Xi_t = [0;0;0;];
+Xi_t = [0;0;0];
 Ks = 0.05;
 
 http_init('');
@@ -27,7 +27,7 @@ http_delete([host '/group/group1']);
 g1 = http_post([host '/group'],gr1);
 g1 = [host '/group/group1'];
 
-Delta_t = 1;
+Delta_t = 1.5;
 P.x = 2340;
 P.y = 1600;
 P.th = 0;
@@ -38,12 +38,11 @@ Map = [];
 %http_put([host '/motion/pose'],P);
 leitura = http_get(g1);
 Pose_R = [leitura{2}.pose.x; leitura{2}.pose.y; NormAngle(leitura{2}.pose.th*pi/180)];
-Xt = [ 0 0 0 ];
+Xt = [ 0; 0; 0 ];
 Xt(1) = Pose_R(1);
 Xt(2) = Pose_R(2);
 Xt(3) = Pose_R(3);
 while true
-
   Delta_S = ((leitura{4}.vel2.right+leitura{4}.vel2.left)*(Delta_t))/2;
   Delta_th = ((leitura{4}.vel2.right-leitura{4}.vel2.left)*(Delta_t))/2*b;
   G_t = [ 1 0 -1*Delta_S*sin(Pose_R(3)+(Delta_th/2)) ; 0 1 Delta_S*cos(Pose_R(3)+(Delta_th/2)); 0 0 1];   
@@ -66,26 +65,29 @@ while true
       Map_aux = [(Pose_R(1)) ; (Pose_R(2))] + [(f(k,1)*cos(f(k,2)+Pose_R(3))) ; (f(k,1)*sin(f(k,2)+Pose_R(3)))];
       found = false;
       if length(Xt) > 3
-        for j=1:2:length(Map(:,1))
-          if sqrt((Map(j,1)-Map_aux(1))^2+((Map(j,2)-Map_aux(2))^2)) <= 200 # Euclidian distance test
+        for j=1:2:length(Xt)-3
+          if sqrt((Xt(3+j)-Map_aux(1))^2+((Xt(3+j+1)-Map_aux(2))^2)) <= 200 # Euclidian distance test
             found = true;
-            Map(j,1) = Map_aux(1);
-            Map(j,2) = Map_aux(2);
-            Xt(3+j) = Map_aux(1);
-            Xt(3+j+1) = Map_aux(2);
+            %Map(j,1) = Map_aux(1);
+            %Map(j,2) = Map_aux(2);
+            %Xt(3+j) = Map_aux(1);
+            %Xt(3+j+1) = Map_aux(2);
             Fxi = zeros(5,length(Xt));
             Fxi(1,1) = 1; Fxi(2,2) = 1; Fxi(3,3) = 1;
             Fxi(4,3+j) = 1; Fxi(5,3+j+1) = 1;
-
+            xi = Xt(3+j);
+            yi = Xt(3+j+1);
           endif
         endfor
       endif
       if found == false
-        Map = [Map; Map_aux'];
+        %Map = [Map; Map_aux'];
         len = length(Xt);
-        Xt = resize(Xt, len+2);
+        %Xt = resize(Xt, len+2);
         Xt(len+1) = Map_aux(1);
         Xt(len+2) = Map_aux(2);
+        xi = Xt(len+1);
+        xy = Xt(len+2);
         Sigma = resize(Sigma, len+2);
         Sigma(len+1,len+1) = Q_t(1,1);
         Sigma(len+2,len+2) = Q_t(1,1);
@@ -93,7 +95,7 @@ while true
         Fxi(1,1) = 1; Fxi(2,2) = 1; Fxi(3,3) = 1;
         Fxi(4,len+1) = 1; Fxi(5,len+2) = 1;
       endif
-      d = [Map_aux(1)-Xt(1); Map_aux(2)-Xt(2)];
+      d = [xi-Xt(1); xy-Xt(2)];
       q = d'*d;
       Ht = (1/q)*[-1*sqrt(q)*d(1) -1*sqrt(q)*d(2) 0 sqrt(q)*d(1) sqrt(q)*d(2); d(2) -1*d(1) -1*q -1*d(2) d(1)]*Fxi;
       Kt = Sigma*Ht'*inv((Ht*Sigma*Ht')+Q_t);
@@ -112,9 +114,9 @@ while true
     dp.y = DeltaP(2);
     dp.th = DeltaP(3)*180/pi;
     dp
-    if (sqrt(dp.x^2 + dp.y^2) <= 200)
-      http_post([host '/motion/pose'],dp);
-    endif
+    %if (sqrt(dp.x^2 + dp.y^2) <= 200)
+    http_post([host '/motion/pose'],dp);
+    %endif
     fflush(stdout);
    endif
 endwhile
