fe18d4cbb25f4e30561838a4d44a0e3c847c4467
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-06-21 22:48:39 -0300

    table version with stopped robot ... finally !!!

--- Atividade4/ekf_slam.m
+++ Atividade4/ekf_slam.m
@@ -2,7 +2,7 @@ close all; clear;
 
 
 addpath('~/Desktop/APIs/Matlab');
-host = 'http://192.168.0.102:4950';
+host = 'http://192.168.0.105:4950';
 laser = '/perception/laser/1/distances?range=-90:90:1';
 
 gr1.name = 'group1';
@@ -35,9 +35,9 @@ P.y = 1600;
 P.th = 0;
 
 Delta_t = 1;
-Map = [];
 
-%http_put([host '/motion/pose'],P);
+
+http_put([host '/motion/pose'],P);
 leitura = http_get(g1);
 Pose_R = [leitura{2}.pose.x; leitura{2}.pose.y; NormAngle(leitura{2}.pose.th*pi/180)];
 Xt = [ 0; 0; 0 ];
@@ -45,6 +45,7 @@ Xt(1) = Pose_R(1);
 Xt(2) = Pose_R(2);
 Xt(3) = Pose_R(3);
 while true
+  leitura = http_get(g1);
   Delta_S = ((leitura{4}.vel2.right+leitura{4}.vel2.left)*(Delta_t))/2;
   Delta_th = ((leitura{4}.vel2.right-leitura{4}.vel2.left)*(Delta_t))/2*b;
   G_t = [ 1 0 -1*Delta_S*sin(Pose_R(3)+(Delta_th/2)) ; 0 1 Delta_S*cos(Pose_R(3)+(Delta_th/2)); 0 0 1];   
@@ -54,7 +55,7 @@ while true
   V4 = 0.5*sin(Pose_R(3)+(Delta_th/2)) - (Delta_S/4*b)*cos(Pose_R(3)+(Delta_th/2));
   V_t = [V1 V2; V3 V4; (1/2*b) (-1/2*b)];
   Sigma_dt = [Ks*abs(leitura{4}.vel2.right*Delta_t) 0; 0 Ks*abs(leitura{4}.vel2.left*Delta_t)];
-  Sigma(1:3,1:3) = (G_t* Sigma(1:3,1:3)*G_t') + (V_t* Sigma_dt *V_t') + R_t;  
+  Sigma(1:3,1:3) = (G_t* Sigma(1:3,1:3)*G_t') + (V_t* Sigma_dt *V_t') + R_t;
   pause(Delta_t);
   leitura = http_get(g1);
   Pose_R = [leitura{2}.pose.x; leitura{2}.pose.y; NormAngle(leitura{2}.pose.th*pi/180)];
@@ -67,18 +68,14 @@ while true
       Map_aux = [(Pose_R(1)) ; (Pose_R(2))] + [(f(k,1)*cos(f(k,2)+Pose_R(3))) ; (f(k,1)*sin(f(k,2)+Pose_R(3)))];
       found = false;
       if length(Xt) > 3
-        for j=1:2:length(Xt)-3
-          if sqrt((Xt(3+j)-Map_aux(1))^2+((Xt(3+j+1)-Map_aux(2))^2)) <= 200 # Euclidian distance test
+        for j=4:2:length(Xt)
+          if sqrt((Xt(j)-Map_aux(1))^2+((Xt(j+1)-Map_aux(2))^2)) <= 200 # Euclidian distance test
             found = true;
-            %Map(j,1) = Map_aux(1);
-            %Map(j,2) = Map_aux(2);
-            %Xt(3+j) = Map_aux(1);
-            %Xt(3+j+1) = Map_aux(2);
             Fxi = zeros(5,length(Xt));
             Fxi(1,1) = 1; Fxi(2,2) = 1; Fxi(3,3) = 1;
-            Fxi(4,3+j) = 1; Fxi(5,3+j+1) = 1;
-            xi = Xt(3+j);
-            yi = Xt(3+j+1);
+            Fxi(4,j) = 1; Fxi(5,j+1) = 1;
+            xi = Xt(j);
+            yi = Xt(j+1);
           endif
         endfor
       endif
@@ -89,23 +86,25 @@ while true
         Xt(len+1) = Map_aux(1);
         Xt(len+2) = Map_aux(2);
         xi = Xt(len+1);
-        xy = Xt(len+2);
+        yi = Xt(len+2);
         Sigma = resize(Sigma, len+2);
         Sigma(len+1,len+1) = Q_t(1,1);
         Sigma(len+2,len+2) = Q_t(1,1);
-        Fxi = zeros(5,length(Xt));
-        Fxi(1,1) = 1; Fxi(2,2) = 1; Fxi(3,3) = 1;
-        Fxi(4,len+1) = 1; Fxi(5,len+2) = 1;
+        
+      else 
+        d = [xi-Xt(1); yi-Xt(2)];
+        q = d'*d;
+        Ht = [];
+        Ht = (1/q)*[-1*sqrt(q)*d(1) -1*sqrt(q)*d(2) 0 sqrt(q)*d(1) sqrt(q)*d(2); d(2) -1*d(1) -1*q -1*d(2) d(1)]*Fxi;
+        Kt = Sigma*Ht'*inv((Ht*Sigma*Ht')+Q_t);
+        Zt = [sqrt(q); NormAngle(NormAngle(atan2(d(2),d(1)))-Xt(3))];
+        Zit = [f(k,1);NormAngle(f(k,2))];
+        INOVA = Zit - Zt;
+        INOVA(2) = NormAngle(INOVA(2));
+        Xt = Xt + Kt*INOVA;
+        Xt(3) = NormAngle(Xt(3));
+        Sigma = (eye(length(Xt)) - (Kt*Ht))*Sigma;
       endif
-      d = [xi-Xt(1); xy-Xt(2)];
-      q = d'*d;
-      Ht = (1/q)*[-1*sqrt(q)*d(1) -1*sqrt(q)*d(2) 0 sqrt(q)*d(1) sqrt(q)*d(2); d(2) -1*d(1) -1*q -1*d(2) d(1)]*Fxi;
-      Kt = Sigma*Ht'*inv((Ht*Sigma*Ht')+Q_t);
-      Zt = [sqrt(q); atan2(d(2),d(1))-Xt(3)];
-      Zit = [f(k,1);f(k,2)];
-      INOVA = Zit - Zt;
-      Xt = Xt + Kt*INOVA;
-      Sigma = (eye(length(Xt)) - (Kt*Ht))*Sigma;
     endfor
     # Pose Update
     Pose_K = [Xt(1);Xt(2);Xt(3)];
@@ -114,11 +113,10 @@ while true
     DeltaP = Pose_K - Pose_R;
     dp.x = DeltaP(1);
     dp.y = DeltaP(2);
-    dp.th = DeltaP(3)*180/pi;
+    dp.th = NormAngle(DeltaP(3));
+    dp.th = dp.th*180/pi;
     dp
-    %if (sqrt(dp.x^2 + dp.y^2) <= 200)
     http_post([host '/motion/pose'],dp);
-    %endif
     fflush(stdout);
    endif
 endwhile
